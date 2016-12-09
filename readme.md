c6c13f5aff7270dffbc1dc15055188f9fe95f65c
Thomas Darimont <thomas.darimont@gmail.com>
2016-12-09 23:13:31 +0100

    PoC for using http2

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Screen Casting App
-Creates screenshots from the display of the server and exposes them via HTTP.
+Creates screenshots from the display of the server and exposes them via HTTP(S).
 
 ## Build 
 ```
@@ -11,7 +11,12 @@ mvn clean package
 java -jar target/screen-casting-app.jar
 ```
 
+## Run with HTTP2
+```
+java -Xbootclasspath/p:./alpn-boot-8.1.10.v20161026.jar -jar target/screen-casting-app.jar
+```
+
 ## Configuration
 
-The screen-casting-app listens on port 9999 by default. One can customize the port by setting the  
+The screen-casting-app listens on port 8443 (https) by default. One can customize the port by setting the  
 `server.port` system property, e.g. via `-Dserver.port=1234`. 
--- src/main/java/de/asw/apps/screencast/ScreenCastApp.java
+++ src/main/java/de/asw/apps/screencast/ScreenCastApp.java
@@ -1,34 +1,46 @@
 package de.asw.apps.screencast;
 
-import java.net.InetAddress;
-
+import io.undertow.UndertowOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.net.InetAddress;
 
 @EnableScheduling
 @SpringBootApplication
 public class ScreenCastApp {
 
-	@Value("${server.port}")
-	String serverPort;
+    @Value("${server.port}")
+    String serverPort;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.setProperty("java.awt.headless", "false");
+        System.setProperty("java.awt.headless", "false");
 
-		SpringApplication.run(ScreenCastApp.class, args);
-	}
+        SpringApplication.run(ScreenCastApp.class, args);
+    }
 
-	@EventListener
-	public void run(ApplicationReadyEvent are) throws Exception {
+    @Bean
+    UndertowEmbeddedServletContainerFactory embeddedServletContainerFactory() {
+        UndertowEmbeddedServletContainerFactory factory = new UndertowEmbeddedServletContainerFactory();
+        factory.addBuilderCustomizers(
+                builder -> builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true));
+        return factory;
+    }
 
-		String hostName = InetAddress.getLocalHost().getHostName();
-		System.out.println("########################################################>");
-		System.out.printf("####### ScreenCasting running under: http://%s:%s/%n", hostName, serverPort);
-		System.out.println("########################################################>");
-	}
+
+    @EventListener
+    public void run(ApplicationReadyEvent are) throws Exception {
+
+        String hostName = InetAddress.getLocalHost().getHostName();
+        System.out.println("########################################################>");
+        System.out.printf("####### ScreenCasting running under: http://%s:%s/%n", hostName, serverPort);
+        System.out.println("########################################################>");
+    }
 }
